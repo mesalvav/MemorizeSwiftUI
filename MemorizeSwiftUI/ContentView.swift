beb24c56b5d98e0d62a4b16d06e7793ca78776da
mario salvatierra <msalv006@fiu.edu>
2020-06-07 10:14:42 -0400

    Initial Commit

---
+++ MemorizeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemorizeSwiftUI
+//
+//  Created by mariosalvatierra on 6/7/20.
+//  Copyright © 2020 mariosalvatierra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
